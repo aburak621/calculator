02719088e241b8c45135731d691fc924b831262a
Aydin Burak "Kakule" Kuscu <a.brkkuscu@gmail.com>
2022-08-07 19:08:12 +0300

    Implement display functionality without dot

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Variables
 const calculator = document.querySelector('.calculator');
 const numbers = calculator.querySelectorAll('.num');
 const display = calculator.querySelector('.display');
@@ -5,15 +6,23 @@ const operators = calculator.querySelectorAll('.operator');
 const equals = calculator.querySelector('.equals');
 const ac = calculator.querySelector('.ac');
 const c = calculator.querySelector('.c');
+const digits = calculator.querySelectorAll('.digit');
 
-const keys = ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/', '%', 'Enter', 'Backspace', 'Delete']
+const keys = ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/', '%', 'Enter', 'Backspace', 'Delete'];
+const segmentCodes = ['1111110', '0110000', '1101101', '1111001', '0110011', '1011011', '1011111', '1110000', '1111111', '1111011'];
+const segmentColorOn = getComputedStyle(document.documentElement).getPropertyValue('--segment-color-on');
+const segmentColorOff = getComputedStyle(document.documentElement).getPropertyValue('--segment-color-off');
 
 let displayValue = '0';
 let previousValue = '';
 let currentValue = '0';
 let currentOperator = '';
+let segments = [];
 
-// TODO: 7-segment display
+for (let i = 0; i < digits.length; i++) {
+    segments[i] = digits[i].querySelectorAll('div');
+}
+printValue();
 
 document.onkeydown = (e) => {
     if (keys.includes(e.key)) {
@@ -84,21 +93,39 @@ function equalsClick(e) {
 }
 
 function printValue() {
-    display.textContent = displayValue;
+    clearDisplay();
+    const displayDigits = displayValue.split('').reverse();
+    for (let i = displayDigits.length - 1; i >= 0; i--) {
+        for (let j = 0; j < 7; j++) {
+            if (segmentCodes[displayDigits[i]].charAt(j) === '1') {
+                segments[i][j].style.backgroundColor = segmentColorOn;
+            } else {
+                segments[i][j].style.backgroundColor = segmentColorOff;
+            }
+        }
+    }
+}
+
+function clearDisplay() {
+    segments.forEach((digit) => {
+        digit.forEach((segment) => {
+            segment.style.backgroundColor = segmentColorOff;
+        });
+    });
 }
 
 function operate(operator, a, b) {
     switch (operator) {
         case '+':
-            return add(a, b);
+            return add(a, b).toString();
         case '-':
-            return subtract(a, b);
+            return subtract(a, b).toString();
         case '*':
-            return multiply(a, b);
+            return multiply(a, b).toString();
         case '/':
-            return divide(a, b);
+            return divide(a, b).toString();
         case '%':
-            return remainder(a, b);
+            return remainder(a, b).toString();
 
         default:
             break;
