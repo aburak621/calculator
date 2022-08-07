409374c14cb93c4cf7d2a2f27408807821b4f1a3
Aydin Burak "Kakule" Kuscu <a.brkkuscu@gmail.com>
2022-08-07 12:16:59 +0300

    Add chaining and deletion buttons

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ const numbers = calculator.querySelectorAll('.num');
 const display = calculator.querySelector('.display');
 const operators = calculator.querySelectorAll('.operator');
 const equals = calculator.querySelector('.equals');
+const ac = calculator.querySelector('.ac');
+const c = calculator.querySelector('.c');
 
-let displayValue = '';
+let displayValue = '0';
 let previousValue = '';
+let currentValue = '';
 let currentOperator = '';
+
+// TODO: 7-segment display
+// TODO: Keyboard support
+// TODO: Prevent double dots
 
 numbers.forEach((element) => {
     element.addEventListener('click', numberClick);
@@ -15,21 +22,46 @@ operators.forEach((element) => {
     element.addEventListener('click', operatorClick);
 });
 equals.addEventListener('click', equalsClick);
+ac.addEventListener('click', () => {
+    displayValue = '0';
+    previousValue = '';
+    currentValue = '';
+    currentOperator = '';
+});
+c.addEventListener('click', () => {
+    displayValue = '0';
+    currentValue = '';
+    printValue();
+});
 
 function numberClick(e) {
-    displayValue += e.target.textContent;
+    if (currentValue === '0') {
+        currentValue = '';
+    }
+    currentValue += e.target.textContent;
+    displayValue = currentValue;
     printValue();
 }
 
 function operatorClick(e) {
+    currentValue = '';
+    if (previousValue !== '') {
+        displayValue = operate(currentOperator, previousValue, displayValue);
+        printValue();
+    }
     previousValue = displayValue;
     displayValue = '';
     currentOperator = e.target.textContent;
 }
 
 function equalsClick(e) {
+    if (currentOperator === '') {
+        return;
+    }
     displayValue = operate(currentOperator, previousValue, displayValue);
-    console.log(displayValue);
+    previousValue = '';
+    currentValue = '';
+    currentOperator = '';
     printValue();
 }
 
@@ -41,19 +73,14 @@ function operate(operator, a, b) {
     switch (operator) {
         case '+':
             return add(a, b);
-            break;
         case '-':
             return subtract(a, b);
-            break;
         case '*':
             return multiply(a, b);
-            break;
         case '/':
             return divide(a, b);
-            break;
         case '%':
             return remainder(a, b);
-            break;
 
         default:
             break;
